eab7137892f99ece7cbf3e06ff817dbcd766a540
GNDavydov <57598627+GNDavydov@users.noreply.github.com>
2020-12-29 16:49:10 +0300

    Add files via upload

--- slae_solution.cpp
+++ slae_solution.cpp
@@ -1,17 +1,161 @@
-#include "slae_solution.hpp"
+﻿#include "slae_solution.hpp"
 #include <limits>
 
 // количество строк в возвращаемом векторе = количество зависимых переменных
 // если пусто, то нет решений
 std::vector<std::vector<double>> get_SLAE_solution(std::vector<std::vector<double>> A, std::vector<double> B) {
 
-    // ...
+    for (size_t j = 0; j < A[0].size(); ++j) {
+
+        size_t i = find_nonzero_element(A, j);
+
+        if (i != -1) {
+            change_string(A, B, i, j);
+            division(A, B, j, j);
+            subtraction_strings(A, B, j, j);
+        }
+
+    }
+
+    if (check_compatibility(A, B)) {
+
+        find_base_variables(A, B);
+
+        std::vector<std::vector<double>> A_B(A.size(), std::vector<double>(A[0].size() + 1, 0));
+
+        for (size_t i = 0; i < A_B.size(); ++i) {
+            for (size_t j = 0; j < A_B[0].size(); ++j) {
+                if (j == A_B[0].size() - 1) {
+                    A_B[i][j] = B[i];
+                }
+                else {
+                    A_B[i][j] = A[i][j];
+                }
+            }
+        }
+        return A_B;
+    }
 
     return std::vector<std::vector<double>>();
 }
 
+size_t find_nonzero_element(std::vector<std::vector<double>>& A, size_t j) {
 
-//////////////////////////////////////////////////
+    for (size_t i = j; i < A.size(); ++i) {
+        if (A[i][j] != 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void change_string(std::vector<std::vector<double>>& A, std::vector<double>& B, size_t i, size_t j) {
+
+    for (size_t k = 0; k < A[i].size(); ++k) {
+        double a = A[j][k];
+        A[j][k] = A[i][k];
+        A[i][k] = a;
+    }
+
+    double b = B[j];
+    B[j] = B[i];
+    B[i] = b;
+}
+
+void division(std::vector<std::vector<double>>& A, std::vector<double>& B, size_t j, size_t index) {
+    double a = A[j][index];
+
+    for (size_t k = 0; k < A[j].size(); ++k) {
+        A[j][k] /= a;
+    }
+
+    B[j] /= a;
+}
+
+void subtraction_strings(std::vector<std::vector<double>>& A, std::vector<double>& B, size_t j, size_t index) {
+
+    for (size_t i = 0; i < A.size(); ++i) {
+
+        double coefficient = A[i][index];
+
+        if (i == j) {
+            continue;
+        }
+
+        for (size_t k = 0; k < A[j].size(); ++k) {
+            A[i][k] -= coefficient * A[j][k];
+            if (fabs(A[i][k]) < 0.00000001) {
+                A[i][k] = 0;
+            }
+        }
+
+        B[i] -= coefficient * B[j];
+        if (fabs(B[i]) < 0.00000001) {
+            B[i] = 0;
+        }
+    }
+
+}
+
+bool check_compatibility(std::vector<std::vector<double>>& A, std::vector<double>& B) {
+
+    for (size_t i = 0; i < A.size(); ++i) {
+
+        size_t count = 0;
+
+        for (size_t j = 0; j < A[0].size(); ++j) {
+
+            if (A[i][j] == 0) {
+                count++;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (count == A[0].size()) {
+
+            if (B[i] != 0) {
+                return false;
+            }
+
+            A.erase(A.begin() + i);
+            B.erase(B.begin() + i);
+            --i;
+        }
+    }
+
+    return true;
+}
+
+void find_base_variables(std::vector<std::vector<double>>& A, std::vector<double>& B) {
+
+    std::unordered_set <size_t> base_variables;
+
+    for (size_t i = 0; i < A.size(); ++i) {
+
+        if (A[i][i] != 0 and base_variables.find(i) == base_variables.end()) {
+            base_variables.insert(i);
+        }
+        else {
+            for (size_t j = i; j < A[i].size(); ++j) {
+
+                if (A[i][j] != 0 and base_variables.find(j) == base_variables.end()) {
+                    base_variables.insert(j);
+                }
+            }
+        }
+    }
+
+    auto iter = base_variables.begin();
+
+    for (size_t i = 0; i < A.size(); ++i) {
+        division(A, B, i, *iter);
+        subtraction_strings(A, B, i, *iter);
+        iter++;
+    }
+}
 
 int get_number_int(const std::string& name) {
 
@@ -26,7 +170,8 @@ int get_number_int(const std::string& name) {
             good_input = false;
             std::cout << "Bad_value. It should be integer. Try again!" << std::endl;
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        } else {
+        }
+        else {
             good_input = true;
         }
     }
@@ -46,7 +191,8 @@ double get_number_double(const std::string& name) {
             good_input = false;
             std::cout << "Bad_value. It should be real number. Try again!" << std::endl;
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        } else {
+        }
+        else {
             good_input = true;
         }
     }
@@ -94,7 +240,7 @@ void print_answer(const std::vector<std::vector<double>>& solution_SLAE) {
     std::cout << "Answer" << std::endl;
 
     // если пусто, то нет решений
-    if(solution_SLAE.empty()) {
+    if (solution_SLAE.empty()) {
         std::cout << "No solution" << std::endl;
         return;
     }
@@ -123,13 +269,15 @@ void print_answer(const std::vector<std::vector<double>>& solution_SLAE) {
                 ++base_vars_cnt;
                 ++line;
                 ++column;
-            } else {
+            }
+            else {
                 // нашли свободную
                 free_vars[column] = ci;
                 ++ci;
                 ++column;
             }
-        } else {
+        }
+        else {
             // вышли из матрицы, но прошлись не по всем переменным
             // остались свободные
             // здесь line <= column < free_vars.size
@@ -165,7 +313,7 @@ void print_answer(const std::vector<std::vector<double>>& solution_SLAE) {
 
     // вывод свободных переменных
     for (size_t i = 0; i < free_vars.size(); ++i) {
-        if(free_vars[i] != 0) {
+        if (free_vars[i] != 0) {
             std::cout << "x_" << (i + 1) << " = c" << free_vars[i] << std::endl;
         }
     }
--- slae_solution.hpp
+++ slae_solution.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 // solve SLAE
 std::vector<std::vector<double>> get_SLAE_solution(std::vector<std::vector<double>> A, std::vector<double> B);
@@ -27,5 +28,23 @@ void print_answer(const std::vector<std::vector<double>>& solution_SLAE);
 
 // show all system
 void print_SLAE(const std::vector<std::vector<double>>& A, const std::vector<double>& B);
+
+// Find a nonzero element in the j-th row
+size_t find_nonzero_element(std::vector<std::vector<double>>& A, size_t j);
+
+// Swapping lines
+void change_string(std::vector<std::vector<double>>& A, std::vector<double>& B, size_t i, size_t j);
+
+// Divide a string by a number
+void division(std::vector<std::vector<double>>& A, std::vector<double>& B, size_t j, size_t index);
+
+// Subtract one line from another
+void subtraction_strings(std::vector<std::vector<double>>& A, std::vector<double>& B, size_t j, size_t index);
+
+// Matrix compatibility check
+bool check_compatibility(std::vector<std::vector<double>>& A, std::vector<double>& B);
+
+// Bringing it to the form: one line one basic variable
+void find_base_variables(std::vector<std::vector<double>>& A, std::vector<double>& B);
 
 #endif // SLAE_SOLUTION_HPP
